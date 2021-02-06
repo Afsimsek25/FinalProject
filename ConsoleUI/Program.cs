549353326717585c81e7c1353125218d042ef9d7
Ahmet Furkan ŞİMŞEK <asimsekfurkan@yandex.com>
2021-02-07 00:01:31 +0300

    6 Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,28 +7,35 @@ namespace ConsoleUI
 {
     //SOLID 
     //Open Closed Principle  ( Yaptığın yazılıma yeni bir özellik ekliyorsan mevcuttaki hiç bir kodunu değiştirmene gerek yok)
+    // ilerde IoC ile new lemeyi bırakacağız.
+    // DTO = Data Transformation Object
     class Program
     {
         static void Main(string[] args)
         {
-            // ProductTest();
-            // ilerde IoC ile new lemeyi bırakacağız.
+            ProductTest();
+            // CategoryTest();
+
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
 
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / "  +product.CategoryName);
             }
         }
     }
