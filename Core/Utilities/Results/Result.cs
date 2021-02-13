641e08044f085bd0b244c72a98cd6047160be0b3
Ahmet Furkan ŞİMŞEK <asimsekfurkan@yandex.com>
2021-02-13 18:44:41 +0300

    10 şubat

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        private bool v1;
+        private string v2;
+
+        // DRY 
+        public Result(bool success, string message):this (success)
+        {
+            // get read onlydir ama constructur içinde set edilebilir
+            Message = message;
+          
+        }
+        public Result(bool success)
+        {       
+            Success = success;
+        }
+
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
