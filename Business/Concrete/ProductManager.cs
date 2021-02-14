2329dfc999d243116c96dd784be991d69255cf39
Ahmet Furkan ŞİMŞEK <asimsekfurkan@yandex.com>
2021-02-14 13:16:45 +0300

    14.02

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,43 +20,40 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
         public IResult Add(Product product)
         {
+            //business codes
 
-            //business codes 
-
-            if (product.ProductName.Length<2)
+            if (product.ProductName.Length < 2)
             {
-
                 //magic strings
-                return new ErrorResult(Messages.productNameınvalid);
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             _productDal.Add(product);
-           
-            return new Result(true, Messages.ProductAdded);   // bunu yapabilmenin yöntemi bir tane constructor eklemektir
+
+            return new SuccessResult(Messages.ProductAdded);
         }
+
 
         public IDataResult<List<Product>> GetAll()
         {
-            //iş kodları 
-            //Yetkisi ver mı?
-
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
 
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
-        public IDataResult< List<Product>> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=> p.CategoryId==id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p=> p.ProductId==productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
@@ -66,12 +63,11 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return new SuccessDataResult<List<ProductDetailDto>> (_productDal.GetProductDetails());
-        }
-
-        IResult IProductService.Add(Product product)
-        {
-            throw new NotImplementedException();
+            if (DateTime.Now.Hour == 23)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,8 +8,11 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
-        public static string productNameınvalid = "Ürün İsmi Geçersiz";
-        public static string MaintenanceTime = "Bakım Zamanı";
-        public static string ProductListed = " Ürünler Listelendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+
+
+    
     }
 }
